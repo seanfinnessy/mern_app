4d093098016c5024ee3de4b7cfca8d2a9e2ca3e0
seanfinnessy <seanfinnessy@icloud.com>
2021-05-27 14:27:23 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+// look at underlying env and see if they declared a port for us (heroku, prod) to use, otherwise use value 5000 (development)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
