afa6f8d29bfb10c578617528b1cf9a3d33ca9632
seanfinnessy <seanfinnessy@icloud.com>
2021-07-06 17:21:32 -0400

    set up authentication

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+
+// order of require statements MATTER! Passport.js uses User.js, so we must load User.js first.
+require('./models/User');
+require('./services/passport');
+
+// connect to our mongoDB
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// authRoutes returns a function... with the second set of () it immediately calls the function we called in. app is passed into that arrow function.
+require('./routes/authRoutes')(app);
 
 // look at underlying env and see if they declared a port for us (heroku, prod) to use, otherwise use value 5000 (development)
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT); 
---
+++ routes/authRoutes.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+
+module.exports = (app) => {
+  // 1. Attempt to authenticate the user that is visiting this route, and use the strategy called google. Scope specifies what you want from their account.
+  app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  // 2. Google sees in the url that we already have the code. Authenticate will see this and exchange it with an actual profile.
+  app.get(
+    '/auth/google/callback', 
+    passport.authenticate('google')
+  )
+  
+  // logout() is a function that is automatically attached to the request object by passport. It takes the cookie and kils the id in it.
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  // req is incoming request, res is outgoing response
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.session);
+    // res.send(req.user);
+  });
+
+};
