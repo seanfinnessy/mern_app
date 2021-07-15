5149ab1110061e65e6588fab5161d29d3c10a776
seanfinnessy <seanfinnessy@icloud.com>
2021-07-15 15:53:21 -0400

    added proxy: true for google strategy

--- services/passport.js
+++ services/passport.js
@@ -1,9 +1,9 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
-const mongoose = require('mongoose');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+const mongoose = require("mongoose");
 
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
 // "user" is what we pulled out of the database 2 seconds ago. user.id is using the id assigned to the mongoDB record that is automatically generated.
 // creates cookie with user id from mongo.
@@ -13,10 +13,9 @@ passport.serializeUser((user, done) => {
 
 // we get whatever had been in the cookie, so we just need id. once we turn the id back into user by searching all our users for the specified id, we call "done".
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then(user => {
-      done(null, user);
-    });
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
 });
 
 // callback url is for where the user is redirected to AFTER being authenticated.
@@ -26,26 +25,26 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
+      callbackURL: "/auth/google/callback",
+      proxy: true, // if request runs thru proxy, change from http to https
     },
     (accessToken, refreshToken, profile, done) => {
       // find the first record inside User collection with a googleId of profile.id.
       // the query returns a Promise (tool we use with JS to handle asynchronous code).
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-          if (existingUser) {
-            // we already have a record with the given profileId.
-            // call "done", 1st arg is error, 2nd arg is the user record.
-            done(null, existingUser);
-          } else {
-            // we dont have a user record with this id, make a new record.
-            // new model instance of a user, then save.
-            // then the database returns a Promise with a model instance of the user, call "done" with that more "fresh" user.
-            new User({ googleId: profile.id })
-              .save()
-              .then(user => done(null, user));
-          }
-        });
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // we already have a record with the given profileId.
+          // call "done", 1st arg is error, 2nd arg is the user record.
+          done(null, existingUser);
+        } else {
+          // we dont have a user record with this id, make a new record.
+          // new model instance of a user, then save.
+          // then the database returns a Promise with a model instance of the user, call "done" with that more "fresh" user.
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user));
+        }
+      });
     }
   )
 );
