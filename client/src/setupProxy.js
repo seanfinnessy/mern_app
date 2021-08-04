9c9e8e4a1c44395b37037a76c64c6084229c5562
seanfinnessy <seanfinnessy@icloud.com>
2021-08-04 13:05:59 -0400

    test

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,6 @@
 // FOR DEV: if anyone tries to access /auth/google or /api on our client side, automatically forward that req to localhost:5000.
 // FOR PROD: CRA bundles everything together and this proxy isnt necessary, the CRA server isnt even used. Only used for good development experience.
+// test
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function(app) {
     app.use(
