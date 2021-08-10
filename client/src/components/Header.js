f7256e3a2865c804a032bb197c2dda90fc8e7c2f
seanfinnessy <seanfinnessy@icloud.com>
2021-08-10 16:25:18 -0400

    added payment, partially

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,32 +1,42 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import Payments from "./Payments";
 
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
       case null:
         return;
+
       case false:
         return (
           <li>
             <a href="/auth/google">Login With Google</a>
           </li>
         );
+
       default:
-        return (
+        return [
+          <li>
+            <Payments />
+          </li>,
           <li>
             <a href="/api/logout">Logout</a>
-          </li>
-        );
+          </li>,
+        ];
     }
   }
+
   render() {
     console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
-          <Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
             Emaily
           </Link>
           <ul className="right">{this.renderContent()}</ul>
